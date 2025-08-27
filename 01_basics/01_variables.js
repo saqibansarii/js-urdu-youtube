0da54febebbc47237746d804490a228516ec4f8c
saqibansarii <najams146@gmail.com>
2025-08-27 07:38:20 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "saqib@gmail.com"
+var accountPassword = "12345"
+accountCity = "Multan"
+let accountState
+
+
+// accountId = 2
+
+accountEmail = "hc@gmail.com"
+accountPassword = "23423424"
+accountCity = "Lahore"
+
+
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
