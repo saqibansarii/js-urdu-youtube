b87184ee09a731ba3f26f341c1444021fee0a9cf
saqibansarii <najams146@gmail.com>
2025-08-27 07:34:10 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //It treat all the JS Code as newer version.
+
+// alert(3 + 3) //we are using node-js, not browser.
+
+// console.log(3 + 3)
+
+// console.log("saqib")
+
+
+let name = "saqib"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined)
