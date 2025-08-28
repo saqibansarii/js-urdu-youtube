bdd1856c9d421782c4be26a19db3f27a27ec8565
saqibansarii <najams146@gmail.com>
2025-08-28 06:04:42 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "saqib"
+
+// console.log(typeof score);
+// console.log(typeof (score))
+
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//if we convert string "33" to number it can easily converted into a number.  "33" => 33.
+// if we convert string "33asd" to number it give us not a number. "33asd" => NaN.
+//if we take boolean value it convert true =>  1 and false => 0.
+
+
+let isLoggedIn = "saqib"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//if we take 1 in boolean it is considered as true and 0 as false. 1 => true, 0 => false.
+// if we take empty string it convert it into false. "" => false.
+// if we take string boolean convert it into true. "saqib" => true.
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof (stringNumber))
